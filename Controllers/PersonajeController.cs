3cb0250ab12a5a7152162dbed04d27df9aae7eec
hipogrifo010 <46372474+hipogrifo010@users.noreply.github.com>
2022-08-20 19:52:05 -0300

    En Construccion
    es necesario utilizar Add-migration /update-database ,se creara en la instancia de sql server express. tal vez sea necesario cambiar le string de la database

---
+++ Controllers/PersonajeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiRestAlchemy.Database;
+using ApiRestAlchemy.Models;
+using DB;
+using System.Net;
+using System.Security.Cryptography;
+using System.Web.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiRestAlchemy.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonajeController : ControllerBase
+    {
+        private DatabaseContext _context;
+
+        public PersonajeController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("/characters")]
+        public async Task<ActionResult<IEnumerable<PersonajeDTO>>> GetCharactersToDTO()
+        {
+            return await _context.Personajes
+                .Select(x => PersonajeToDTO(x))
+                .ToListAsync();
+        }
+
+
+        [HttpGet("/movies")]
+        public async Task<ActionResult<IEnumerable<PeliculaOserieDTO>>> GetPeliculasToDTO()
+        {
+            return await _context.PeliculasOseries
+                .Select(x => PeliculaOserieToDTO(x))
+                .ToListAsync();
+        }
+
+
+
+        private static PersonajeDTO PersonajeToDTO(Personaje todoItem) =>
+        new PersonajeDTO
+        {
+          Nombre = todoItem.Nombre,
+          Imagen = todoItem.Imagen
+        };
+        private static PeliculaOserieDTO PeliculaOserieToDTO(PeliculaOserie peliculaOserie) =>
+        new PeliculaOserieDTO
+        {
+           Titulo= peliculaOserie.Titulo,
+           Imagen= peliculaOserie.Imagen,
+           FechaDeCreacion= peliculaOserie.FechaDeCreacion
+        };
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+using ApiRestAlchemy.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+   // 
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DatabaseContext>
+    (options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseContext")); });
+
+var app = builder.Build();
+
+
+using (var scope =app.Services.CreateScope())
+{
+    var context=scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    context.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
